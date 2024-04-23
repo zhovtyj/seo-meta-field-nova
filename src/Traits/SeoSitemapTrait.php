34957e75281f2bb9de41071c266aac913f0a5ef1
zhovtyj <igorzhovtyj@gmail.com>
2024-04-23 15:11:02 +0300

    Update SeoSitemapTrait.php

--- src/Traits/SeoSitemapTrait.php
+++ src/Traits/SeoSitemapTrait.php
@@ -8,7 +8,7 @@ trait SeoSitemapTrait
 
     public function getSitemapItemLastModified(){
         if(isset($this->updated_at) || isset($this->created_at)) {
-            return isset($this->updated_at) ? $this->updated_at : $this->created_at;
+            return isset($this->updated_at) ? $this->updated_at->format('Y-m-d\TH:i:sP') : $this->created_at->format('Y-m-d\TH:i:sP');
         }
         return null;
     }
